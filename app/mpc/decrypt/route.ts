023439e8a20fdd9e75520d402251d5badab7fabe
idsignhq <jeanclaude.aoun@hotmail.com>
2024-10-16 18:29:43 +0000

    Monorepo Update (ee23e43): Merge pull request #66 from IDsignHQ:dev

--- app/mpc/decrypt/route.ts
+++ app/mpc/decrypt/route.ts
@@ -1,11 +1,34 @@
 import { NextResponse } from 'next/server'
 import { decrypt } from '@toruslabs/eccrypto'
+import { ethers } from 'ethers'
+import {Address, Cell, contractAddress, loadStateInit } from '@ton/ton'
 
 export async function POST(request: Request) {
 	if(!process.env.NODE_PRIVATE_KEY){
 		return NextResponse.json({ error: 'Node private key is required' }, { status: 400 })
 	}
-	const { encryptedShare } = await request.json()
+	const { payload, encryptedShare } = await request.json()
+
+	if(payload.chain === 'TON'){
+		const stateInit = loadStateInit(Cell.fromBase64(payload.signature).beginParse())
+		const wantedAddress = Address.parse(payload.rawaddress)
+		const address = contractAddress(wantedAddress.workChain, stateInit)
+		if (!address.equals(wantedAddress)) {
+			return NextResponse.json({ error: 'Invalid Signature' }, { status: 401 })
+		}
+	} else { // EVM
+		const recoveredAddr = ethers.verifyMessage(payload.timestamp.toString(), payload.signature)
+		if (recoveredAddr !== payload.address) {
+			return NextResponse.json({ error: 'Invalid Signature' }, { status: 401 })
+		}
+	}
+
+	if (Date.now() > (payload.timestamp + 5*60*1000)) { return NextResponse.json({ error: 'Error: Signature expired' }, { status: 401 })}
+
+	const authorized = encryptedShare.acls.includes(payload.address)
+	if(!authorized) {
+		return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+	}
 	
 	const decryptedShare = await decrypt(Buffer.from(process.env.NODE_PRIVATE_KEY, 'base64'), {
 		iv: Buffer.from(encryptedShare.iv, 'base64'),
